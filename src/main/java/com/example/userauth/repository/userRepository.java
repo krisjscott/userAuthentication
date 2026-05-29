622c7044f2e6c742c7f9b17c07d606e627c3b812
Krisjscott <krishkumar6566@gmail.com>
2026-05-30 03:12:52 +0530

    Have set up JwtService for creating tokens and checking it's expiration

---
+++ src/main/java/com/example/userauth/repository/userRepository.java
@@ -0,0 +1,15 @@
+package com.example.userauth.repository;
+
+import com.example.userauth.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface userRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByMail(String email);
+
+    Optional<User> findByVerification(String verification);
+}
