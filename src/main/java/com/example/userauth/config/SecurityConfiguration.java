bc3d39e3ec2085dc15f2b84fc96a66f460d84a40
Krisjscott <krishkumar6566@gmail.com>
2026-06-01 13:32:12 +0530

    Added Security checker for the user

--- src/main/java/com/example/userauth/config/SecurityConfiguration.java
+++ src/main/java/com/example/userauth/config/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.example.userauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
